915088bacd51c007f472a16386be858c29f35e80
David Ackerman <ackermand@janelia.hhmi.org>
2020-03-31 15:56:23 -0400

    simple algorithm seems to work well enough

--- agglomerate.py
+++ agglomerate.py
@@ -3,21 +3,31 @@ import daisy
 import numpy as np
 from scipy import ndimage
 
-test_roi = daisy.Roi((0, 0, 0), (501, 501, 501))
-test_file = 'test_mito.n5'
-fragments_ds = 'actual_fragments'
-dist_max_ds = 'actual_dist_max'
-thresholds = [0.01, 0.05, 0.1, 0.2, 0.3]
+start_xyz = [6500, 100, 3500]#[6900, 100, 6600] #
+dims_xyz = [1500, 700, 1700]#[500,500,500]#
+
+#start_xyz = [6900, 100, 6600] #[6500, 100, 3500]#[6900, 100, 6600] #
+#dims_xyz = [500,500,500]#[1500, 700, 1700]#
+
+test_roi = daisy.Roi((start_xyz[2]*4, start_xyz[1]*4, start_xyz[0]*4), (dims_xyz[2]*4, dims_xyz[1]*4, dims_xyz[0]*4) )# daisy.Roi((500*4, 100*4, 5500*4), (2500*4, 500*4, 2500*4) )#daisy.Roi((6600*4, 100*4, 6900*4), (500*4, 500*4, 500*4) ) #(3500, 100, 7000), (4000, 600, 7500))
+
+test_file = f'/groups/cosem/cosem/ackermand/SegmentationFromJan/new_thresh_{start_xyz[2]}_{start_xyz[1]}_{start_xyz[0]}_x_{dims_xyz[2]}_{dims_xyz[1]}_{dims_xyz[0]}.n5'
+
+fragments_ds = 'fragments'
 
 if __name__ == '__main__':
+    #original method
+    thresholds = [0.5, 0.4, 0.3, 0.2, 0.1]
+
+    dist_ds = 'dist_max' #'dist_max'
     print("Reading fragments and distances...")
     fragments = daisy.open_ds(test_file, fragments_ds)[test_roi]
-    dist_max = daisy.open_ds(test_file, dist_max_ds)[test_roi]
+    dist_ds = daisy.open_ds(test_file, dist_ds)[test_roi]
     fragments.materialize()
-    dist_max.materialize()
+    dist_ds.materialize()
 
     print("Creating affinity arrays...")
-    affs = np.stack([dist_max.data]*3)
+    affs = np.stack([dist_ds.data]*3)
     print(f"{affs.shape}")
 
     print("Agglomerating...")
@@ -25,13 +35,19 @@ if __name__ == '__main__':
     for s, t in zip(waterz.agglomerate(
         affs=affs,
         fragments=fragments.data,
-        thresholds=inverted_thresholds), thresholds):
+        thresholds=inverted_thresholds,
+        scoring_function = 1 - waterz.QuantileAffinity(50)), 
+        #score = penalty. fuse if penalty is low.  
+        #scoring_function = 1.0 - waterz.QuantileAffinity(90)/np.tanh( waterz.ContactArea())),#1.0 - waterz.ContactArea() ), #np.tanh( waterz.ContactArea() ) ), #TANH IS JUST A PLACEHOLDER!!!!
+        thresholds):
 
-        print("Storing segmentation...")
+        print(f"Storing segmentation for threshold {t}...")
         segmentation_ds = daisy.prepare_ds(
             test_file,
-            'actual_segmentation_%.3f'%t,
+            'updated_segmentation_%.3f'%t,
             total_roi=fragments.roi,
             voxel_size=fragments.voxel_size,
+            compressor={'id': 'gzip', 'level': 6},
+            write_size=[180*4,180*4,180*4],
             dtype=np.uint64)
         segmentation_ds[test_roi] = s
--- watershed.py
+++ watershed.py
@@ -1,14 +1,18 @@
-from scipy.ndimage import gaussian_filter, label
+from scipy.ndimage import gaussian_filter, label, distance_transform_edt
 from skimage.feature import peak_local_max
 from skimage.morphology import watershed
 import daisy
 import numpy as np
+import copy
 
-test_roi = daisy.Roi((0, 0, 0), (501, 501, 501))
+start_xyz = [6500, 100, 3500]#[6900, 100, 6600] #
+dims_xyz = [1500, 700, 1700]#[500,500,500]#
 
-test_file = '/groups/scicompsoft/home/ackermand/Programming/SegmentationFromJan/test_mito.n5'
-mito_ds = 'actual_distance_transform_masked'
-#er_ds = 'er'
+test_roi = daisy.Roi((start_xyz[2]*4, start_xyz[1]*4, start_xyz[0]*4), (dims_xyz[2]*4, dims_xyz[1]*4, dims_xyz[0]*4) )# daisy.Roi((500*4, 100*4, 5500*4), (2500*4, 500*4, 2500*4) )#daisy.Roi((6600*4, 100*4, 6900*4), (500*4, 500*4, 500*4) ) #(3500, 100, 7000), (4000, 600, 7500))
+
+test_file = '/nrs/cosem/cosem/training/v0003.2/setup25/HeLa_Cell3_4x4x4nm/HeLa_Cell3_4x4x4nm_it600000.n5'
+output_file = f'/groups/cosem/cosem/ackermand/SegmentationFromJan/new_thresh_{start_xyz[2]}_{start_xyz[1]}_{start_xyz[0]}_x_{dims_xyz[2]}_{dims_xyz[1]}_{dims_xyz[0]}.n5'
+mito_ds = 'mito'
 
 def find_seeds(dist, sigma=3.0):
 
@@ -19,33 +23,45 @@ def normalize_distances(array):
 
     array.data = array.data.astype(np.float32)/255.0
     array.data -= 126/255.0
-    np.clip(array.data, 0, 0.5, out=array.data)
+    array.data[array.data<0] = 0
+    #array.data[array.data<127] = 0
+   
+    #array.data[(array.data<=225) & (array.data>=127)] = 0.5
+    #array.data[array.data>225] = 1
+    #array.data -= 126
+    #array.data /= 255
+    #array.data[array.data<127] = 0
+    #array.data[array.data>=127] = 1
+    #array.data = distance_transform_edt(array.data)
+    #np.clip(array.data, 0, 0.5, out=array.data)
+
+def calculate_dist_in_voxels(array):
+    array.data[array.data==255] = 254 #To prevent infinity
+    array.data = np.arctanh( (array.data-127.0)/128.0 ) *50.0/4
+    print(np.amax(array.data))
+    array.data[array.data<0] = 0 
 
 if __name__ == '__main__':
 
     print("Opening datasets...")
-    #dist_er = daisy.open_ds(test_file, er_ds)
     dist_mitos = daisy.open_ds(test_file, mito_ds)
 
     print(f"Got distances in {dist_mitos.roi}")
-    #dist_er = dist_er[test_roi]
     dist_mitos = dist_mitos[test_roi]
 
     print("Reading into memory...")
-    #dist_er.materialize()
     dist_mitos.materialize()
 
     print("Normalizing distances...")
-    #normalize_distances(dist_er)
     normalize_distances(dist_mitos)
 
     # TODO: include all classes of the same level in the hierarchy
     print("Computing max distances...")
-    #dist_max = np.abs(np.maximum(dist_er.data, dist_mitos.data))
     dist_max = np.abs(dist_mitos.data)
     
     print("Finding seeds...")
     seeds = find_seeds(dist_max)
+    print(f"Num seeds: {np.count_nonzero(seeds)}")
 
     print("Running watershed...")
     print(f"dist_max: {dist_max.shape}")
@@ -58,8 +74,8 @@ if __name__ == '__main__':
         voxel_size=dist_mitos.voxel_size)
 
     fragments_ds = daisy.prepare_ds(
-        'test_mito.n5',
-        'actual_fragments',
+        output_file,
+        'fragments',
         total_roi=fragments.roi,
         voxel_size=fragments.voxel_size,
         dtype=np.uint64)
@@ -68,11 +84,35 @@ if __name__ == '__main__':
     fragments_ds[test_roi] = fragments
 
     dist_max_ds = daisy.prepare_ds(
-        'test_mito.n5',
-        'actual_dist_max',
+        output_file,
+        'dist_max',
         total_roi=fragments.roi,
         voxel_size=fragments.voxel_size,
         dtype=np.float32)
 
     print("Storing dist_max...")
     dist_max_ds[test_roi] = dist_max
+
+
+    # ## due to memory issue, read in again
+    # print("Opening datasets...")
+    # dist_mitos = daisy.open_ds(test_file, mito_ds)
+
+    # print(f"Got distances in {dist_mitos.roi}")
+    # dist_mitos = dist_mitos[test_roi]
+
+    # print("Reading into memory...")
+    # dist_mitos.materialize()
+
+    # print("Calculate dist in voxels...")
+    # calculate_dist_in_voxels(dist_mitos)
+
+    # dist_mitos_ds = daisy.prepare_ds(
+    #     output_file,
+    #     'dist_mitos_in_voxels',
+    #     total_roi=fragments.roi,
+    #     voxel_size=fragments.voxel_size,
+    #     dtype=np.float32)
+
+    # print("Storing dist_in_voxels")
+    # dist_mitos_ds[test_roi] = dist_mitos
